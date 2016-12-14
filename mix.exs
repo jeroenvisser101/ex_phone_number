e2b93de73bf9fcaf3f41da4542c35a25df0d8688
Jeroen Visser <jeroenvisser101@gmail.com>
2016-12-14 17:48:45 +0100

    Add package to hex.pm

--- mix.exs
+++ mix.exs
@@ -3,13 +3,29 @@ defmodule ExPhoneNumber.Mixfile do
 
   def project do
     [app: :ex_phone_number,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.3.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      test_coverage: [tool: ExCoveralls],
      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test],
-     deps: deps]
+     deps: deps,
+     description: description,
+     package: package]
+  end
+
+  def package do
+    [files: ["lib", "config", "resources", "README*", "mix.exs"],
+     maintainers: ["Jose Miguel Rivero Bruno (@josemrb)"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/socialpaymentsbv/ex_phone_number"}]
+  end
+
+  def description do
+    """
+    A library for parsing, formatting, and validating international phone
+    numbers. Based on Google's [libphonenumber](https://github.com/googlei18n/libphonenumber).
+    """
   end
 
   def application do
@@ -21,7 +37,8 @@ defmodule ExPhoneNumber.Mixfile do
       {:sweet_xml, "~> 0.6.1"},
       {:ex_spec, "~> 2.0", only: :test},
       {:excoveralls, "~> 0.5.6", only: :test},
-      {:credo, "~> 0.4.11", only: [:dev, :test]}
+      {:credo, "~> 0.4.11", only: [:dev, :test]},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
